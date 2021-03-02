3efb5e7f4add55354cd9d8c7a186372c06b4c76c
kuzojman <74131284+kuzojman@users.noreply.github.com>
2021-03-02 19:29:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ from flask_migrate import Migrate
 ####from data import teachers_info
 import json
 import pandas as pd
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+import psycopg2
 
 
+
+
+app = Flask(__name__)
+app.secret_key = "randomstring"
 
 categories_from_excel = pd.read_excel('database_download.xlsx', sheet_name = 'categories')
 meals_from_excel = pd.read_excel('database_download.xlsx', sheet_name = 'meals')
 
-print(len(meals_from_excel["title"]))
 
-print((categories_from_excel["title"]))
+##print((categories_from_excel["title"]))
 #print(meals_from_excel["title"])
 
 
@@ -20,7 +26,9 @@ print((categories_from_excel["title"]))
 app = Flask(__name__)
 app.secret_key = "randomstring"
 # Это создаст базу в оперативной памяти, которая очистится после завершения программы.
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:Vovik20121985@localhost/for_third_stepic"
+
+ ##   "sqlite:///test.db"
 ####app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -29,6 +37,14 @@ migrate = Migrate(app, db)
 
 #db.create_all()
 
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String)
+    password = db.Column(db.String)
+    orders = db.relationship("Order", back_populates='user')
 
 
 ####!!!!!!!!!!!!!!!!!!!!!!#########
@@ -46,6 +62,8 @@ class Order(db.Model):
     mail = db.Column(db.String)
     phone = db.Column(db.String)
     adresses = db.Column(db.String)
+    user = db.relationship("User")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     meals = db.relationship(
         "Meals", secondary=association_table, back_populates="orders"
     )
@@ -59,14 +77,30 @@ class Meals(db.Model):
     price = db.Column(db.Float)
     description = db.Column(db.String, unique=True)
     picture = db.Column(db.String, unique=True)
-    category = db.Column(db.Float)
+    category = db.relationship("Category")
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     orders = db.relationship(
         "Order", secondary=association_table, back_populates="meals"
     )
 
 
-for i in range(len(meals_from_excel["title"])):
-    print((meals_from_excel["title"])[i],(meals_from_excel["description"])[i])
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    meals = db.relationship("Meals", back_populates='category')
+
+
+#for i in range(len(categories_from_excel["title"])):
+#    categ = Category(title=(categories_from_excel["title"])[i],
+ #                    meals=(categories_from_excel["id"])[i])
+
+
+#db.session.add(categ)
+#db.session.commit()
+
+
 
 
 
@@ -74,12 +108,12 @@ for i in range(len(meals_from_excel["title"])):
 #for i in range(len(meals_from_excel["title"])):
 #    dish = Meals(title=(meals_from_excel["title"])[i],
 #                     description=(meals_from_excel["description"])[i],
- #                     picture=(meals_from_excel["picture"])[i],
+#                      picture=(meals_from_excel["picture"])[i],
  #                     price=(meals_from_excel["price"])[i],
- #                      category=(meals_from_excel["category_id"])[i])
-
-#db.session.add(dish)
-#db.session.commit()
+#                    category_id=int((meals_from_excel["category_id"])[i])
+#                   )
+#    db.session.add(dish)
+#    db.session.commit()
 
 
 
@@ -93,13 +127,30 @@ for i in range(len(meals_from_excel["title"])):
 #db.session.add(teacher)
 #db.session.commit()
 
-dishes = Meals.query.all()
-
-for dish in dishes:
-    print(dish.title+"  "+str(dish.id))
 
 
 
 
+#categ = Category.query.all()
+#print(type(categ))
+
+
+#for cat in categ:
+ #   print(str(cat.id))
+
+
+
+
+#dishes = Meals.query.all()
+
+#for dish in dishes:
+#    print(dish.title+" "+str(dish.category_id))
+
+#admin = Admin(app)
+#admin.add_view(ModelView(User, db.session))
+#admin.add_view(ModelView(Category, db.session))
+#admin.add_view(ModelView(Meals, db.session))
+
+#app.run(debug=True)
 
 
